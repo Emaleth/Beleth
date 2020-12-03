5f5cc6d2af33469ba6ccd93c384643856b20017f
Emaleth <Emaleth@protonmail.com>
2020-12-03 19:10:53 +0100

    improved character controller, fixed slope slide

--- src/character/Character.gd
+++ src/character/Character.gd
@@ -1,51 +1,67 @@
 extends KinematicBody
 
-
 var speed = 7
-var acceleration = 20
-var gravity = 9.8
-var jump = 5
+var h_acceleration = 6
+var air_acceleration = 1
+var normal_acceleration = 6
+var gravity = 20
+var jump = 8
+var full_contact = false
 
 var mouse_sensitivity = 0.3
 
 var direction = Vector3()
-var velocity = Vector3()
-var fall = Vector3()
+var h_velocity = Vector3()
+var movement = Vector3()
+var gravity_vec = Vector3()
 
 onready var head = $Head
-
+onready var ground_check = $GroundCheck
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
-	
+
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
 		head.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
-		head.rotation.x = clamp(head.rotation.x, deg2rad(-90), deg2rad(90))
+		head.rotation.x = clamp(head.rotation.x, deg2rad(-89), deg2rad(89))
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		
-	if Input.is_action_just_pressed("jump"):
-		fall.y = jump
+func _physics_process(delta):
 	
-		
-func _process(delta):
-	direction = Vector3.ZERO
+	direction = Vector3()
+	
+	full_contact = ground_check.is_colliding()
 	
 	if not is_on_floor():
-		fall.y -= gravity * delta
+		gravity_vec += Vector3.DOWN * gravity * delta
+		h_acceleration = air_acceleration
+	elif is_on_floor() and full_contact:
+		gravity_vec = -get_floor_normal() * gravity
+		h_acceleration = normal_acceleration
+	else:
+		gravity_vec = -get_floor_normal()
+		h_acceleration = normal_acceleration
 		
-		
-	direction += transform.basis.z * (Input.get_action_strength("move_back") - Input.get_action_strength("move_forward"))
-	direction += transform.basis.x * (Input.get_action_strength("move_right") - Input.get_action_strength("move_left"))
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or ground_check.is_colliding()):
+		gravity_vec = Vector3.UP * jump
+	
+	if Input.is_action_pressed("move_forward"):
+		direction -= transform.basis.z
+	elif Input.is_action_pressed("move_backward"):
+		direction += transform.basis.z
+	if Input.is_action_pressed("move_left"):
+		direction -= transform.basis.x
+	elif Input.is_action_pressed("move_right"):
+		direction += transform.basis.x
 	
 	direction = direction.normalized()
+	h_velocity = h_velocity.linear_interpolate(direction * speed, h_acceleration * delta)
+	movement.z = h_velocity.z + gravity_vec.z
+	movement.x = h_velocity.x + gravity_vec.x
+	movement.y = gravity_vec.y
 	
-	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
-	velocity = move_and_slide(velocity + fall, Vector3.UP)
-
-	
+	move_and_slide(movement, Vector3.UP)
 
