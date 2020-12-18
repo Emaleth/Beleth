e839a3a5293dd74e14f6e7b3f97e268c1fbc0d42
Emaleth <Emaleth@protonmail.com>
2020-12-18 02:37:49 +0100

    blender made nail model

--- src/weapons/sr_nightfall/sr_Nightfall.gd
+++ src/weapons/sr_nightfall/sr_Nightfall.gd
@@ -4,15 +4,15 @@ extends "res://src/weapons/_weapon/_Weapon.gd"
 func _ready():
 	sight_mat = preload("res://resources/materials/sights/triangle_sight.tres")
 	bullet_decal = preload("res://src/decals/Nail.tscn")
-	sfx = preload("res://assets/sounds/sfx/cannon_01.wav")
-	fire_rate = 5 # 50 bullets per second is maximum
+	sfx = preload("res://assets/sounds/sfx/bang_03.wav")
+	fire_rate = 2 # 50 bullets per second is maximum
 	damage = 1
-	permited_modes = [fire_mode.BURST, fire_mode.SEMI]
-	default_mode = fire_mode.SEMI
+	permited_modes = [fire_mode.AUTO, fire_mode.BURST, fire_mode.SEMI]
+	default_mode = fire_mode.AUTO
 	clip_size = 320000
 	recoil_force = Vector3(0.05, 0.5, 0.1)
-	spread = 0.01
-#	slug_size = 8
+	spread = 3.0
+	slug_size = 72
 #	akimbo = true
 	slider = $Nightfall/Slider
 	slider_mov_dist = 0.120
